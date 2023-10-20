ca164f85a48856c8826eacb1f6b184c47ba168e3
Иван Агабалаев <agabalaev01@yandex.ru>
2023-10-20 15:38:10 +0300

    Add GroupName and IsChecked properties to a custom radio button

--- ScheduleX/MVVM/View/Components/IconTextRadioButton.xaml.cs
+++ ScheduleX/MVVM/View/Components/IconTextRadioButton.xaml.cs
@@ -2,41 +2,68 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace ScheduleX.MVVM.View.Components
+namespace ScheduleX.MVVM.View.Components;
+
+public partial class IconTextRadioButton : UserControl
 {
-    public partial class IconTextRadioButton : UserControl
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(UserControl),
+            new FrameworkPropertyMetadata(null)
+        );
+
+    public static readonly DependencyProperty IconGeometryProperty =
+        DependencyProperty.Register(
+            "IconGeometry",
+            typeof(Geometry),
+            typeof(UserControl),
+            new FrameworkPropertyMetadata(null)
+        );
+
+    public static readonly DependencyProperty GroupNameProperty =
+        DependencyProperty.Register(
+            "GroupName",
+            typeof(string),
+            typeof(UserControl),
+            new FrameworkPropertyMetadata(null)
+        );
+
+    public static readonly DependencyProperty IsCheckedProperty =
+        DependencyProperty.Register(
+            "IsChecked",
+            typeof(bool),
+            typeof(UserControl),
+            new FrameworkPropertyMetadata(null)
+        );
+
+    public string Text
     {
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register(
-                "Text",
-                typeof(string),
-                typeof(UserControl),
-                new FrameworkPropertyMetadata(null)
-            );
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
 
-        public static readonly DependencyProperty IconGeometryProperty =
-            DependencyProperty.Register(
-                "IconGeometry",
-                typeof(Geometry),
-                typeof(UserControl),
-                new FrameworkPropertyMetadata(null)
-            );
+    public Geometry IconGeometry
+    {
+        get => (Geometry)GetValue(IconGeometryProperty);
+        set => SetValue(IconGeometryProperty, value);
+    }
 
-        public string Text
-        {
-            get => (string)GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
-        }
+    public string GroupName
+    {
+        get => (string)GetValue(GroupNameProperty);
+        set => SetValue(GroupNameProperty, value);
+    }
 
-        public Geometry IconGeometry
-        {
-            get => (Geometry)GetValue(IconGeometryProperty);
-            set => SetValue(IconGeometryProperty, value);
-        }
+    public bool IsChecked
+    {
+        get => (bool)GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
 
-        public IconTextRadioButton()
-        {
-            InitializeComponent();
-        }
+    public IconTextRadioButton()
+    {
+        InitializeComponent();
     }
 }
